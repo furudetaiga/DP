1097e3956ad6a5039ca5d58565ba22acf8058c7b
furudetaiga <furudetaiga@gmail.com>
2017-01-26 23:47:35 +0900

    Add files via upload

---
+++ DP.c
@@ -0,0 +1,125 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#define MaxNum 20
+
+double x[MaxNum];
+double y[MaxNum];
+int num;
+int path[MaxNum]={0};
+int dist(int , int);
+
+int min(int , int);
+
+int DP(int , int, int **);
+
+void FindTheWay(int , int, int **);
+
+int main(int argc, char **argv){
+  char *fname;
+  FILE *fp1, *fp2;
+  double data;
+  int c;
+  int count=0;
+  int now = 0;
+  int visited = 0;
+  int i = 0;
+  int **length;
+
+  if(argc!=2){
+    fprintf(stderr, "Usage:%s prob_file\n", argv[0]);
+    exit(1);
+  }
+  fname = argv[1];
+
+  fp1 = fopen(fname,"r");
+  if(fp1 == NULL){
+    printf("error opening file!\n");
+    return 1;
+  }
+
+  c = fscanf(fp1, "%lf", &data);
+  while(c != EOF){
+    count += 1;
+    if(count == 1){
+      num = data;
+    }else if(count % 2 == 0){
+      x[count/2 - 1] = data;
+    }else if(count % 2 == 1){
+      y[count/2 - 1] = data;
+    }
+    c = fscanf(fp1, "%lf", &data);
+  }
+  fclose(fp1);
+  printf("x[0]=%d", dist(0 ,4));
+
+  length = malloc(sizeof(int)*MaxNum*((1<<(MaxNum-1))-1));
+  
+  printf("chinaamini \n");
+  DP(now, visited, length);
+  
+  FindTheWay(now, visited, length);
+  
+  fp2 = fopen("DP_TPS", "w");
+  if(fp2 == NULL){
+    printf("error opening file!\n");
+    return 1;
+  }
+  for(i = 0; i<num; i++){
+    fprintf(fp2, "%d ", path[i]);
+  }
+  fclose(fp2);
+  free(length);
+  return 0; 
+}
+
+int dist(int i, int j){
+  double xd, yd;
+  xd = x[i] - x[j];
+  yd = y[i] - y[j];
+  return (int)(sqrt(xd*xd+yd*yd)+0.5);
+}
+
+int DP(int now, int visited, int **length){
+  int country;
+  int res = 99999;
+  printf("ol \n");
+      printf("now= %d, 0= 0", visited);
+  if(visited == (1<<(num-1))-1){
+    printf("fool %d %d\n", now, visited);
+    length[now][visited] = dist(0, now);
+    printf("make \n");
+    return visited;
+  }else{
+    for(country = 1; country < num;country++){
+      printf("hehe\n");
+      if(1<<(country-1) & visited)continue;
+      if(res > dist(now, country) + length[country][DP(country,visited|(1<<(country-1)),length)])
+	printf("cooler \n");
+      res = dist(now, country) + length[country][DP(country,visited|(1<<(country-1)),length)];
+    }
+    printf("noob \n");
+    length[now][visited] = res;
+    return visited;
+  }
+}
+int i = 0;
+void FindTheWay(int now, int visited, int **length){
+  int country;
+  int min = 0;
+  int res = 99999;
+  if(visited == (1<<(num-1))-1){
+    return;
+  }else{
+    for(country = 1; country < num; country++){
+      if(1<<(country-1) & visited)continue;
+      if(length[country][visited|(1<<(country-1))] + dist(now, country) == length[now][visited]){
+	res = length[country][visited|(1<<(country-1))];
+	min = country;
+	break;
+      }
+    }
+    path[++i] = min;
+    return FindTheWay(min, visited|(1<<(min-1)),length);
+  }
+}
